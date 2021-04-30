45e8e67004f45ecc3222dac05f6ac0e48ac021d2
Jaro Habr <jaro@freshbits.ch>
2021-04-30 18:26:58 +0200

    Add constants and update dataset notebook

---
+++ code/constants.py
@@ -0,0 +1,9 @@
+import os
+
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+DATA_DIR = os.path.join(PROJECT_DIR, "data")
+TRAIN_DIR = os.path.join(DATA_DIR, "train")
+VALIDATION_DIR = os.path.join(DATA_DIR, "validation")
+TEST_DIR = os.path.join(DATA_DIR, "test")
+BASELINE_DIR = os.path.join(PROJECT_DIR, "baseline")
+EXPERIMENT_DIR = os.path.join(PROJECT_DIR, "experiment")
