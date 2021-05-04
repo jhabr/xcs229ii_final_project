b8d6b7864d4c5b6691ff150ff776abaf0aecf66c
Jaro Habr <jaro@freshbits.ch>
2021-05-04 23:26:13 +0200

    Add augmentation tests

--- code/test/augmentation_tests.py
+++ code/test/augmentation_tests.py
@@ -7,14 +7,22 @@ from utils.helper import Visualisation
 
 class DataAugmentationTest(unittest.TestCase):
 
-    def test_advanced_augementation(self):
-        image = Trainer().get_training_data(dataset_size=1)["images"][0]
-        augmented_image = DataAugmentation().apply_advanced(image)
-        assert augmented_image.shape == image.shape
-        Visualisation().show(image=augmented_image.squeeze())
+    def setUp(self):
+        self.image = Trainer().get_training_data(dataset_size=1)["images"][0]
 
-    def test_hair_augementation(self):
-        image = Trainer().get_training_data(dataset_size=1)["images"][0]
-        augmented_image = AdvancedHairAugmentation().apply(image)
-        assert augmented_image.shape == image.shape
-        Visualisation().show(image=augmented_image.squeeze())
+    def tearDown(self):
+        self.image = None
+
+    def test_advanced_augmentation(self):
+        augmented_image = DataAugmentation().apply_advanced(self.image)
+        self.assertEqual(augmented_image.shape, self.image.shape)
+        Visualisation().plot_images(image=augmented_image.squeeze())
+
+    def test_hair_augmentation(self):
+        augmented_image = AdvancedHairAugmentation().apply(self.image)
+        self.assertEqual(augmented_image.shape, self.image.shape)
+        Visualisation().plot_images(image=augmented_image.squeeze())
+
+
+if __name__ == '__main__':
+    unittest.main()
