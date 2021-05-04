dea3a1c27310d64be6249cc64d4c5ccce47c6c9b
Jaro Habr <jaro@freshbits.ch>
2021-05-04 23:26:20 +0200

    Add helper methods

--- code/utils/helper.py
+++ code/utils/helper.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 class Visualisation:
 
-    def show(self, **images):
+    def plot_images(self, **images):
         """PLot images in one row."""
         n = len(images)
         plt.figure(figsize=(16, 5))
@@ -13,4 +13,24 @@ class Visualisation:
             plt.yticks([])
             plt.title(' '.join(name.split('_')).title())
             plt.imshow(image)
+        plt.show()
+
+    def plot_curves(self, history):
+        plt.figure(figsize=(30, 5))
+        plt.subplot(121)
+        plt.plot(history.history['iou_score'])
+        plt.plot(history.history['val_iou_score'])
+        plt.title('Model iou_score')
+        plt.ylabel('iou_score')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Validation'], loc='upper left')
+
+        # Plot training & validation loss values
+        plt.subplot(122)
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Validation'], loc='lower left')
         plt.show()
