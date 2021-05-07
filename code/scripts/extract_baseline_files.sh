7c0186ff6d30bade99fd236860e9c2b6174ce1ea
Jaro Habr <jaro@freshbits.ch>
2021-05-07 14:21:55 +0200

    Fix scripts

--- code/scripts/extract_baseline_files.sh
+++ code/scripts/extract_baseline_files.sh
@@ -4,11 +4,11 @@ azure_instance="scpdxcs@ml-lab-2c26d21d-2e78-44d8-a0be-f41571a9502f.southcentral
 project_dir="/home/scpdxcs/projects/xcs229ii_final_project"
 
 # Extract logs from Azure Instance
-scp -P 61999 \
+scp -P 61999 -r \
   ${azure_instance}:${project_dir}/code/logs \
   ~/Downloads/xcs229ii_final_project/logs
 
 # Extract baseline binaries (history, model weights) from Azure Instance
-scp -P 61999 \
+scp -P 61999 -r \
   ${azure_instance}:${project_dir}/code/baseline/export \
   ~/Downloads/xcs229ii_final_project/baseline/export
