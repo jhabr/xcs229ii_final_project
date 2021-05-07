b591a6ae711a60b26f61b18a269e17df1ba7a0e0
Jaro Habr <jaro@freshbits.ch>
2021-05-07 13:18:58 +0200

    Refactorings

--- code/scripts/copy_dataset.sh
+++ code/scripts/copy_dataset.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+azure_instance="scpdxcs@ml-lab-2c26d21d-2e78-44d8-a0be-f41571a9502f.southcentralus.cloudapp.azure.com"
+project_dir="/home/scpdxcs/projects/xcs229ii_final_project"
+
 # Copy dataset to Azure Instance
 scp -P 61999 \
   data.zip \
-  scpdxcs@ml-lab-2c26d21d-2e78-44d8-a0be-f41571a9502f.southcentralus.cloudapp.azure.com:/home/scpdxcs/projects/xcs229ii_final_project/code/data
+  ${azure_instance}:${project_dir}/code/data
---
+++ code/scripts/extract_baseline_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+azure_instance="scpdxcs@ml-lab-2c26d21d-2e78-44d8-a0be-f41571a9502f.southcentralus.cloudapp.azure.com"
+project_dir="/home/scpdxcs/projects/xcs229ii_final_project"
+
+# Extract logs from Azure Instance
+scp -P 61999 \
+  ${azure_instance}:${project_dir}/code/logs \
+  ~/Downloads/xcs229ii_final_project/logs
+
+# Extract baseline binaries (history, model weights) from Azure Instance
+scp -P 61999 \
+  ${azure_instance}:${project_dir}/code/baseline/export \
+  ~/Downloads/xcs229ii_final_project/baseline/export
