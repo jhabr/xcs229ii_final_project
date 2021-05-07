47890918d06ee9f1364e28536e499b0a0e45d708
Jaro Habr <jaro@freshbits.ch>
2021-05-08 01:29:14 +0200

    Add support for full size prediction

--- code/test/dataloader_tests.py
+++ code/test/dataloader_tests.py
@@ -47,6 +47,30 @@ class DataLoaderTests(unittest.TestCase):
             mask=data["masks"][9].squeeze()
         )
 
+    def test_load_original_size_data(self):
+        simple_data_loader = SimpleDataLoader(
+            backbone='resnet34',
+            images_path=os.path.join(TRAIN_DIR, "images"),
+            mask_path=os.path.join(TRAIN_DIR, "masks"),
+            resize=False,
+            size=10
+        )
+
+        train_data = simple_data_loader.get_images_masks()
+        train_image = train_data["images"][0]
+        train_mask = train_data["masks"][0]
+
+        self.assertEqual(train_image.shape, (767, 1022, 3))
+        self.assertEqual(train_mask.shape, (767, 1022, 1))
+        self.assertEqual(train_image.shape[:-1], train_mask.shape[:-1])
+
+        train_image = simple_data_loader.get_images()[0]
+        train_mask = simple_data_loader.get_masks()[0]
+
+        self.assertEqual(train_image.shape, (767, 1022, 3))
+        self.assertEqual(train_mask.shape, (767, 1022, 1))
+        self.assertEqual(train_image.shape[:-1], train_mask.shape[:-1])
+
 
 if __name__ == '__main__':
     unittest.main()
