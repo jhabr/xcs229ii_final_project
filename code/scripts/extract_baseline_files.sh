0fac1218817485389c1f229eb6b139f1e5002779
Jaro Habr <jaro@freshbits.ch>
2021-05-07 16:42:10 +0200

    Update export script

--- code/scripts/extract_baseline_files.sh
+++ code/scripts/extract_baseline_files.sh
@@ -2,13 +2,19 @@
 
 azure_instance="scpdxcs@ml-lab-2c26d21d-2e78-44d8-a0be-f41571a9502f.southcentralus.cloudapp.azure.com"
 project_dir="/home/scpdxcs/projects/xcs229ii_final_project"
+export_datetime=$(date +"%Y_%m_%d_%H%M")
+azure_dir=~/Downloads/xcs229ii_final_project/azure/"${export_datetime}"
 
 # Extract logs from Azure Instance
+mkdir "${azure_dir}"
+
 scp -P 61999 -r \
   ${azure_instance}:${project_dir}/code/logs \
-  ~/Downloads/xcs229ii_final_project/logs
+  "${azure_dir}"
 
 # Extract baseline binaries (history, model weights) from Azure Instance
+mkdir "${azure_dir}"
+
 scp -P 61999 -r \
   ${azure_instance}:${project_dir}/code/baseline/export \
-  ~/Downloads/xcs229ii_final_project/baseline/export
+  "${azure_dir}"/baseline
