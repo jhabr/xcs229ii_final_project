fa60985a05ee41258187607fd454fcab7aba0e96
Jaro Habr <jaro@freshbits.ch>
2021-05-13 16:38:42 +0200

    Add metrics code (from repo)

---
+++ code/test/segmentation_metrics_tests.py
@@ -0,0 +1,46 @@
+import os
+import unittest
+
+from constants import TRAIN_DIR
+from utils.dataloader import SimpleDataLoader
+from utils.metrics import Metrics
+import segmentation_models as sm
+
+
+class SegmentationMetricsTests(unittest.TestCase):
+
+    def setUp(self):
+        self.simple_data_loader = SimpleDataLoader(
+            images_folder_path=os.path.join(TRAIN_DIR, "images"),
+            masks_folder_path=os.path.join(TRAIN_DIR, "masks"),
+            resize_to=(256, 256),
+            size=3
+        )
+        self.model = sm.Unet(activation='sigmoid')
+
+    def tearDown(self):
+        self.simple_data_loader = None
+
+    def test_segmentation_metrics(self):
+        data = self.simple_data_loader.get_images_masks()
+        images = data["images"]
+        masks = data["masks"]
+        self.assertEqual(len(images), 3)
+        self.assertEqual(len(masks), 3)
+
+        metrics = Metrics().calculate(
+            model=self.model,
+            images=images,
+            masks=masks
+        )
+        self.assertEqual(metrics.n_images, 3)
+        self.assertEqual(metrics.jaccard, 0.0)
+        # self.assertEqual(metrics.n_false_negatives, 3)
+        # self.assertEqual(metrics.n_false_positives, 165)
+        # self.assertEqual(metrics.n_pred_labels, 12)
+        # self.assertEqual(metrics.n_true_labels, 12)
+        # self.assertEqual(metrics.n_true_positives, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- code/utils/metrics.py
+++ code/utils/metrics.py
@@ -1,17 +1,19 @@
-import umetrics as um
+import utils.segmentation_metrics as sm
 import numpy as np
 
 
 class Metrics:
 
     def calculate(self, model, images, masks, strict=True, iou_threshold=0.65):
+        assert model is not None and images is not None and masks is not None
+
         metrics = []
 
         for index, image in enumerate(images):
             image = np.expand_dims(image, axis=0)
             predicted_mask = model.predict(image).round().squeeze(axis=0)
             mask = masks[index]
-            results = um.calculate(mask, predicted_mask, strict=strict, iou_threshold=iou_threshold).results
+            results = sm.calculate(mask, predicted_mask, strict=strict, iou_threshold=iou_threshold).results
             metrics.append(results)
 
-        return um.MetricResults.merge(metrics)
+        return sm.MetricResults.merge(metrics)
