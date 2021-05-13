ad8327afa9d3ac019b38f96db1b6f9109d595fe2
Jaro Habr <jaro@freshbits.ch>
2021-05-13 14:41:45 +0200

    Make metrics calculation configurable

--- code/utils/metrics.py
+++ code/utils/metrics.py
@@ -4,14 +4,14 @@ import numpy as np
 
 class Metrics:
 
-    def calculate(self, model, images, masks):
+    def calculate(self, model, images, masks, strict=True, iou_threshold=0.65):
         metrics = []
 
         for index, image in enumerate(images):
             image = np.expand_dims(image, axis=0)
             predicted_mask = model.predict(image).round().squeeze(axis=0)
             mask = masks[index]
-            results = um.calculate(mask, predicted_mask, strict=True, iou_threshold=0.65).results
+            results = um.calculate(mask, predicted_mask, strict=strict, iou_threshold=iou_threshold).results
             metrics.append(results)
 
         return um.MetricResults.merge(metrics)
