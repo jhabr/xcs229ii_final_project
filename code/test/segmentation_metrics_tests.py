088ff1e148fbcfcdcc7fc9b5ee200d41164e48c5
Jaro Habr <jaro@freshbits.ch>
2021-05-14 02:22:20 +0200

    Some refactorings, more tests (wip)

--- code/test/segmentation_metrics_tests.py
+++ code/test/segmentation_metrics_tests.py
@@ -20,11 +20,10 @@ class SegmentationMetricsTests(unittest.TestCase):
             size=3
         )
         self.model = sm.Unet(activation='sigmoid')
-        self.mask = self.__create_mask()
-        self.predicted_mask = self.__create_predicted_mask()
         self.binary_segmentation_metric = BinarySegmentationMetric()
 
-    def __create_mask(self):
+    @property
+    def mask(self):
         mask = np.array(
             [[1., 1., 1., 1., 1.],
              [1., 1., 1., 1., 1.],
@@ -35,7 +34,20 @@ class SegmentationMetricsTests(unittest.TestCase):
         mask = np.expand_dims(mask, axis=2)
         return mask
 
-    def __create_predicted_mask(self):
+    @property
+    def mask2(self):
+        mask2 = np.array(
+            [[0., 0., 0., 0., 0.],
+             [0., 1., 1., 1., 0.],
+             [0., 1., 1., 1., 0.],
+             [0., 1., 1., 1., 0.],
+             [0., 0., 0., 0., 0.]]
+        )
+        mask2 = np.expand_dims(mask2, axis=2)
+        return mask2
+
+    @property
+    def predicted_mask(self):
         predicted_mask = np.array(
             [[1., 1., 1., 1., 1.],  # 5 true positives
              [1., 0., 0., 0., 0.],  # 1 true positive, 4 false negatives
@@ -46,15 +58,26 @@ class SegmentationMetricsTests(unittest.TestCase):
         predicted_mask = np.expand_dims(predicted_mask, axis=2)
         return predicted_mask
 
+    @property
+    def predicted_mask2(self):
+        predicted_mask2 = np.array(
+            [[0., 0., 0., 0., 0.],  # 5 true negatives
+             [0., 1., 1., 1., 0.],  # 2 true negatives, 3 true positives
+             [0., 1., 1., 1., 0.],  # 2 true negatives, 3 true positives
+             [0., 1., 1., 1., 0.],  # 2 true negatives, 3 true positives
+             [0., 0., 0., 0., 0.]]  # 5 true negatives
+        )
+        predicted_mask2 = np.expand_dims(predicted_mask2, axis=2)
+        return predicted_mask2
+
     def tearDown(self):
         self.simple_data_loader = None
         self.model = None
-        self.mask = None
-        self.predicted_mask = None
         self.binary_segmentation_metric = None
 
     def test_segmentation_metrics(self):
         results = self.binary_segmentation_metric.calculate(self.mask, self.predicted_mask)
+        self.assertEqual(results["n_images"], 1)
         self.assertEqual(results["n_true_positives"], 6)
         self.assertEqual(results["n_true_negatives"], 12)
         self.assertEqual(results["n_false_positives"], 3)
@@ -66,6 +89,38 @@ class SegmentationMetricsTests(unittest.TestCase):
         self.assertEqual(results["sensitivity"], 0.6)
         self.assertEqual(results["specificity"], 0.8)
         self.assertEqual(results["accuracy"], 0.72)
+
+    def test_segmentation_metrics_2(self):
+        results = self.binary_segmentation_metric.calculate(self.mask2, self.predicted_mask2)
+        self.assertEqual(results["n_images"], 1)
+        self.assertEqual(results["n_true_positives"], 9)
+        self.assertEqual(results["n_true_negatives"], 16)
+        self.assertEqual(results["n_false_positives"], 0)
+        self.assertEqual(results["n_false_negatives"], 0)
+        self.assertEqual(results["iou"], 1.0)
+        self.assertEqual(results["jaccard"], 1.0)
+        self.assertEqual(results["dice"], 1.0)
+        self.assertEqual(results["f1_score"], 0.5)
+        self.assertEqual(results["sensitivity"], 1.0)
+        self.assertEqual(results["specificity"], 1.0)
+        self.assertEqual(results["accuracy"], 1.0)
+
+    def test_segmentation_metrics_batch(self):
+        masks = np.stack([self.mask, self.mask2])
+        predicted_masks = np.stack([self.predicted_mask, self.predicted_mask2])
+        results = self.binary_segmentation_metric.calculate_batch(masks, predicted_masks)
+        self.assertEqual(results["n_images"], 2)
+        self.assertEqual(results["n_true_positives"], 6 + 9)
+        self.assertEqual(results["n_true_negatives"], 12 + 16)
+        self.assertEqual(results["n_false_positives"], 3 + 0)
+        self.assertEqual(results["n_false_negatives"], 4 + 0)
+        self.assertEqual(results["iou"], (0.46153846153846156 + 1.0) / 2.0)
+        self.assertEqual(results["jaccard"], (0.46153846153846156 + 1.0) / 2.0)
+        self.assertEqual(results["dice"], (0.631578947368421 + 1.0) / 2.0)
+        self.assertEqual(results["f1_score"], (0.3157894736842105 + 0.5) / 2.0)
+        self.assertEqual(results["sensitivity"], (0.6 + 1.0) / 2.0)
+        self.assertEqual(results["specificity"], (0.8 + 1.0) / 2.0)
+        self.assertEqual(results["accuracy"], (0.72 + 1.0) / 2.0)
 
     def test_true_positives(self):
         """TP: pixels correctly segmented as foreground"""
--- code/utils/seg_metrics/core.py
+++ code/utils/seg_metrics/core.py
@@ -4,10 +4,10 @@ import numpy as np
 class BinarySegmentationMetric:
 
     def __init__(self):
-        self.true_positives = 0
-        self.true_negatives = 0
-        self.false_positives = 0
-        self.false_negatives = 0
+        self.true_positives = []
+        self.true_negatives = []
+        self.false_positives = []
+        self.false_negatives = []
 
     def calculate(self, mask, predicted_mask) -> dict:
         self.__calc_simple_metrics(mask, predicted_mask)
@@ -41,10 +41,10 @@ class BinarySegmentationMetric:
 
         results = {
             "n_images": len(masks),
-            "n_true_positives": self.true_positives,
-            "n_true_negatives": self.true_negatives,
-            "n_false_positives": self.false_positives,
-            "n_false_negatives": self.false_negatives,
+            "n_true_positives": self.tp,
+            "n_true_negatives": self.tn,
+            "n_false_positives": self.fp,
+            "n_false_negatives": self.fn,
             "iou": np.mean(iou),
             "jaccard": self.jaccard,
             "dice": self.dice,
@@ -55,8 +55,6 @@ class BinarySegmentationMetric:
         }
 
         return results
-
-
 
     def __calc_simple_metrics(self, mask, predicted_mask):
         assert mask.shape == predicted_mask.shape
@@ -69,41 +67,48 @@ class BinarySegmentationMetric:
 
         height, width = mask.shape
 
+        true_positives, true_negatives, false_positives, false_negatives = 0, 0, 0, 0
+
         for i in range(height):
             for j in range(width):
                 mask_pixel_value = mask[i][j]
                 predicted_mask_pixel_value = predicted_mask[i][j]
                 if mask_pixel_value == predicted_mask_pixel_value:
                     if mask_pixel_value == 1:
-                        self.true_positives += 1
+                        true_positives += 1
                     else:
-                        self.true_negatives += 1
+                        true_negatives += 1
                 else:
                     if predicted_mask_pixel_value == 0:
-                        self.false_negatives += 1
+                        false_negatives += 1
                     else:
-                        self.false_positives += 1
+                        false_positives += 1
 
-        assert self.true_positives + \
-               self.true_negatives + \
-               self.false_positives + \
-               self.false_negatives == height * width
+        assert true_positives + \
+               true_negatives + \
+               false_positives + \
+               false_negatives == height * width
+
+        self.true_positives.append(true_positives)
+        self.true_negatives.append(true_negatives)
+        self.false_positives.append(false_positives)
+        self.false_negatives.append(false_negatives)
 
     @property
     def tp(self):
-        return self.true_positives
+        return sum(self.true_positives)
 
     @property
     def tn(self):
-        return self.true_negatives
+        return sum(self.true_negatives)
 
     @property
     def fp(self):
-        return self.false_positives
+        return sum(self.false_positives)
 
     @property
     def fn(self):
-        return self.false_negatives
+        return sum(self.false_negatives)
 
     def per_object_iou(self, mask, predicted_mask):
         intersection = np.logical_and(mask, predicted_mask)
