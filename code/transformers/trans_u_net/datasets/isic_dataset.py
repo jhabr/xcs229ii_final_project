8c9f20c7d6727a7894fc09d9aede4e1ef5901231
Jaro Habr <jaro@freshbits.ch>
2021-05-22 00:52:55 +0200

    Return pytorch tensor from dataset (permuted)

--- code/transformers/trans_u_net/datasets/isic_dataset.py
+++ code/transformers/trans_u_net/datasets/isic_dataset.py
@@ -1,5 +1,6 @@
 import os
 
+import torch
 from torch.utils.data import Dataset
 
 from constants import TRAIN_DIR
@@ -7,10 +8,10 @@ from utils.dataloader import SimpleDataLoader
 
 
 class ISICDataset(Dataset):
-    def __init__(self, resize_to, size=None):
+    def __init__(self, resize_to, size=None, image_dir=TRAIN_DIR):
         self.train_images_masks = SimpleDataLoader(
-            images_folder_path=os.path.join(TRAIN_DIR, "images"),
-            masks_folder_path=os.path.join(TRAIN_DIR, "masks"),
+            images_folder_path=os.path.join(image_dir, "images"),
+            masks_folder_path=os.path.join(image_dir, "masks"),
             resize_to=resize_to,
             size=size
         ).get_images_masks()
@@ -21,4 +22,6 @@ class ISICDataset(Dataset):
     def __getitem__(self, idx):
         image = self.train_images_masks["images"][idx]
         label = self.train_images_masks["masks"][idx]
-        return {'image': image, 'label': label}
+        image = torch.from_numpy(image).permute(2, 0, 1)
+        label = torch.from_numpy(label).permute(2, 0, 1)
+        return image, label
