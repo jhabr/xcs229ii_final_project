e6361d39a4d63e3927245552f2faf3a03c66b657
Jaro Habr <jaro@freshbits.ch>
2021-05-22 12:42:40 +0200

    Add scripts

--- code/scripts/run_transunet_experiments.sh
+++ code/scripts/run_transunet_experiments.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0
-python3 "$(pwd)/transformers/trans_u_net/train.py" --dataset ISIC --dataset_size 10 --vit_name R50-ViT-B_16
+if [ -z ${PYTHONPATH+x} ]
+then
+  echo "PYTHONPATH not set. Exiting..."; exit 1
+  else echo "PYTHONPATH set to $PYTHONPATH."
+fi
+(python3 "$PYTHONPATH"/experiments/transunet_runner.py 2>&1) >> "$PYTHONPATH"/logs/transunet.log &
